984dc9c24bb6306cc6e9aaea2c15c3b2d7807ba4
jemraider21 <jemraider21@gmail.com>
2024-08-18 19:45:07 -0400

    Created directory structure for attributes as well as a FileAttribute enumeration

---
+++ src/com/attributes/file__attributes.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class FileAttribute(Enum):
+    """
+    An enumeration of common attributes associated with a file
+    """
+    NAME = "file.name"
+    VERSION = "file.version"
+    TYPE = "file.type"
--- src/com/structs/filestruct.py
+++ src/com/structs/filestruct.py
@@ -1,12 +1,15 @@
 class FileStruct:
     """
-    Comment
-    
-    Args:
-        attribute_map(dict(str, str)): A map of attribute names and their values
-        data_bytes(bytearray): The bytes that represent the data
+    A structure that represents an incoming file.
+    Will contain the file's bytes as well as attributes from its metadata.
     """
+
     def __init__(self, attribute_map: dict[str, str], data_bytes: bytearray) -> None:
+        """
+        Args:
+            attribute_map(dict(str, str)): A map of attribute names and their values
+            data_bytes(bytearray): The bytes that represent the data
+        """
         self.__attribute_map = attribute_map
         self.__data_bytes = data_bytes
 
