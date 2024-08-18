51623308382fa0b813389059a350efdcc353bce9
jemraider21 <jemraider21@gmail.com>
2024-08-18 17:29:26 -0400

    Initial commit, created filestruct representation

---
+++ src/com/filestruct.py
@@ -0,0 +1,29 @@
+class FileStruct:
+    """
+    Comment
+    
+    Args:
+        attribute_map(dict(str, str)): A map of attribute names and their values
+        data_bytes(bytearray): The bytes that represent the data
+    """
+    def __init__(self, attribute_map: dict[str, str], data_bytes: bytearray) -> None:
+        self.__attribute_map = attribute_map
+        self.__data_bytes = data_bytes
+
+    def get_attribute_map(self) -> dict[str, str]:
+        """
+        Getter for retrieving the map of attribute names and their values
+        
+        Returns:
+            dict(str, str): The map of attribute names and their values
+        """
+        return self.__attribute_map
+
+    def get_data_bytes(self) -> bytearray:
+        """
+        Getter for retrieving the byte array representing the data
+        
+        Returns:
+            bytearray: The byte array representation of the data
+        """
+        return self.__data_bytes
